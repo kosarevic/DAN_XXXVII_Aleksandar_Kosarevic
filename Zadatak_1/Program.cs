3151cefb646866be71ab5ebf0c72e1cb251cb282
Kosarevic <kosarevic@gmail.com>
2020-06-23 09:32:43 +0200

    Created code for random routs.

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -10,7 +10,15 @@ namespace Zadatak_1
     {
         static void Main(string[] args)
         {
-            
+            int[] routs = new int[1000];
+            Random r = new Random();
+
+            for (int i = 0; i < routs.Length; i++)
+            {
+                routs[i] = r.Next(1, 5000);
+            }
+
+
         }
     }
 }
